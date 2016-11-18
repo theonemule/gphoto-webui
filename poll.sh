7a3cfc9f551e1084939a67cbd6a5a702a83c0d5d
blaize <me@blaize.net>
2016-11-18 16:45:48 +0000

    added comments

--- poll.sh
+++ poll.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+#path where motion writes the files to.
 motion_dir="/mnt/ramdisk"
+
+#the url of the camera services. This is probably OK to leave as is.
 camera_url="http://localhost:9000/service.php?action=takePicture"
 
+#starts the php web server in the background.
 nohup php -S 0.0.0.0:9000 > phpd.log 2>&1 &
 
+#polls the motion dir every half second for changes.
 while true
 do
 	filecount="$(find ${motion_dir} -type f | wc -l)"
-
 	
 	if [ "${filecount}" -ne "0" ]
 	then
-		#do stuff here
-		echo "nuking files"
-		rm $motion_dir/*
-		wget  -qO- $camera_url &> /dev/null
+		rm $motion_dir/* #Removes the files from the motion directory to reset it.
+		wget  -qO- $camera_url &> /dev/null #instructs the web service to take a picture.
 	fi
 	
-	sleep 0.5
+	sleep 0.5 #sleep half a second.
 done
